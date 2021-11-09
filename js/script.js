fcbea87a50a85a8ef974694b70d5e2d85c671e19
Дмитрий Стрельцов <corpsworld@gmail.com>
2021-11-09 15:45:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// "use strict"
+
+const movie = +prompt('сколько фильмов посмотрели в последнее время?','');
+
+const moviesDB = {
+    count: movie,
+    actors: {}
+};
+
+const a = prompt('какой актер играл в главной роли?',''),
+      b = prompt('насколько оцените его игру (1-10) ?',''),
+      c = prompt('какой актер играл в главной роли?',''),
+      d = prompt('насколько оцените его игру (1-10) ?','');
+
+moviesDB.actors[a] = b;
+moviesDB.actors[c] = d;
+
+console.log(moviesDB);
