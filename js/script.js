eee94ab443cf37e3d11bb4540d65d849bc87c665
Дмитрий Стрельцов <corpsworld@gmail.com>
2021-11-26 21:00:43 +0300

    "add function in DB"

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
-"use strict"
+"use strict";
 
-const month = +prompt('сколько фильмов посмотрели в последнее время?','');
+let month;
+
+function start() {
+    month = +prompt('сколько фильмов посмотрели в последнее время?', '');
+
+    while (month == '' || month == null || isNaN(month)) {
+        month = +prompt('сколько фильмов посмотрели в последнее время?', '');
+    }
+}
+start();
 
 const moviesDB = {
     count: month,
-    actors: {}
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-for (let i = 0; i < month; i++) {
-    const property = prompt('какой актер играл в главной роли?',''),
-          value = +prompt('насколько оцените его игру (1-10) ?','');
+function rememberFilms() {
+    for (let i = 0; i < month; i++) {
+        const property = prompt('какой актер играл в главной роли?', ''),
+            value = +prompt('насколько оцените его игру (1-10) ?', '');
     
-    if (property != null && value != null && property != '' && value != '' && property.length < 50) {
-        moviesDB.actors[property] = value;
-    } else {
-        i--;
+        if (property != null && value != null && property != '' && value != '' && property.length < 50) {
+            moviesDB.actors[property] = value;
+        } else {
+            i--;
+        }
+    }
+}
+rememberFilms();
+
+// в hidden попадёт false, а потом поменяется на обратное значение в if и БД выведется, а при исходном true скроется
+function showDB (hidden) {
+    if (!hidden) {
+        console.log(moviesDB);
     }
 }
 
-console.log(moviesDB);
+showDB(moviesDB.privat);
+
+function writeYourGenres () {
+    for (let i = 1; i < 4; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        moviesDB.genres[i - 1] = genre;
+    }
+}
+writeYourGenres ();
