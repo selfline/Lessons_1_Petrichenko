368128ee91775822659d92011e9cc6a1a85155f0
Дмитрий Стрельцов <corpsworld@gmail.com>
2021-12-05 21:23:29 +0300

    add methods in object

--- js/dbFilms.js
+++ js/dbFilms.js
@@ -1,55 +1,62 @@
-let month;
-
-function start() {
-    month = +prompt('сколько фильмов посмотрели в последнее время?', '');
-
-    while (month == '' || month == null || isNaN(month)) {
-        month = +prompt('сколько фильмов посмотрели в последнее время?', '');
-    }
-}
-// start();
+"use strict";
 
 const moviesDB = {
-    count: month,
+    count: 0,
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        moviesDB.count = +prompt('сколько фильмов посмотрели в последнее время?', '');
+    
+        while (moviesDB.count == '' || moviesDB.count == null || isNaN(moviesDB.count)) {
+            moviesDB.count = +prompt('сколько фильмов посмотрели в последнее время?', '');
+        }
+    },
+    rememberFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const property = prompt('какой актер играл в главной роли?', ''),
+                  value = +prompt('насколько оцените его игру (1-10) ?', '');
+        
+            if (property != null && value != null && property != '' && value != '' && property.length < 50) {
+                moviesDB.actors[property] = value;
+            } else {
+                i--;
+            }
+        }
+    },
+    showDB: function(hidden) {
+        if (!hidden) {
+            console.log(moviesDB);
+        }
+    },
+    toggleVisibleDB: function() {
+        if (moviesDB.privat) {
+            moviesDB.privat = false;
+        } else {
+            moviesDB.privat = true;
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i < 4; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (genre === '' || genre == null) {
+                console.log('некорректные данные');
+                i--;
+            } else {
+                moviesDB.genres[i - 1] = genre;
+            }
+        }
+        moviesDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр №${i + 1} - это ${item}`);
+        });
+    }
 };
 
-function rememberFilms() {
-    for (let i = 0; i < month; i++) {
-        const property = prompt('какой актер играл в главной роли?', ''),
-              value = +prompt('насколько оцените его игру (1-10) ?', '');
-    
-        if (property != null && value != null && property != '' && value != '' && property.length < 50) {
-            moviesDB.actors[property] = value;
-        } else {
-            i--;
-        }
-    }
-}
-// rememberFilms();
-
-// в hidden попадёт false, а потом поменяется на обратное значение в if и БД выведется, а при исходном true скроется
-function showDB (hidden) {
-    if (!hidden) {
-        console.log(moviesDB);
-    }
-}
-
-showDB(moviesDB.privat);
+// console.log(moviesDB);
 
 // setTimeout(showDB, 4000, moviesDB.privat);
 
-function writeYourGenres () {
-    for (let i = 1; i < 4; i++) {
-        moviesDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-// writeYourGenres ();
-
-
 // встроенная функция задержки выполнения другой функции (и т.п.?)
-setTimeout(function() {
-    console.log(1);
-}, 3000);
+// setTimeout(function() {
+//     console.log(1);
+// }, 3000);
